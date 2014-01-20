f9685fe4aff64a68fa002050aeac836651e70727
Austin Louden <austinlouden@gmail.com>
2014-01-20 15:27:15 -0500

    start the gamepad

--- walk/ALGameScene.m
+++ walk/ALGameScene.m
@@ -15,12 +15,20 @@
         
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
+        // info labels
         SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Courier-Bold"];
         myLabel.text = @"9:30 (26° F)";
         myLabel.fontSize = 30;
         myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
                                        CGRectGetMidY(self.frame));
         [self addChild:myLabel];
+        
+        // gamepad up down left right
+        SKSpriteNode *upNode = [SKSpriteNode spriteNodeWithColor:[UIColor whiteColor] size:CGSizeMake(15.0f, 15.0f)];
+        upNode.position = CGPointMake(CGRectGetMaxX(self.frame)-30,CGRectGetMaxY(self.frame)-30);
+        upNode.name = @"upNode";
+        [self addChild:upNode];
+        
         
         
         self.player = [SKSpriteNode spriteNodeWithImageNamed:@"back_still"];
@@ -36,7 +44,11 @@
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        SKAction *moveAction = [SKAction moveTo:location duration:1.0];
+        CGFloat xDist = (location.x - self.player.position.x);
+        CGFloat yDist = (location.y - self.player.position.y);
+        CGFloat distance = sqrt((xDist * xDist) + (yDist * yDist));
+        NSLog(@"distance: %f", distance);
+        SKAction *moveAction = [SKAction moveTo:location duration:distance/250.0f];
         [self.player runAction:moveAction];
     }
 }
